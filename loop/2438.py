9d2ded9044607cecdd4403fd46988241656c42d1
dlalstjd0513 <dlalstjd0513@naver.com>
2025-01-22 00:10:33 +0900

    upload baekjoon step-by-step loop part

---
+++ loop/2438.py
@@ -0,0 +1,8 @@
+n = int(input())
+a='*'
+for i in range(n):
+    if i == 0:
+        print(a)
+    else :
+        a= '*' + a
+        print(a)
---
+++ loop/25314.py
@@ -0,0 +1,7 @@
+N = int(input()) #입력
+str = 'int'
+n = N//4
+
+for i in range(n):
+    str = 'long ' + str
+print(str)
